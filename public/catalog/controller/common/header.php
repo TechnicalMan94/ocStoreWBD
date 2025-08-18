d50b1386d8ee2a0f32025263652ef92c70a7b213
Konstantin <site@webdes.by>
2025-08-18 20:15:09 +0300

    Корзина два раза

--- public/catalog/controller/common/header.php
+++ public/catalog/controller/common/header.php
@@ -89,7 +89,7 @@ class ControllerCommonHeader extends Controller {
 		
 		$data['language'] = $this->load->controller('common/language');
 		$data['currency'] = $this->load->controller('common/currency');
-		$data['currency'] = $this->load->controller('common/currency');
+		
 		if ($this->config->get('configblog_blog_menu')) {
 			$data['blog_menu'] = $this->load->controller('blog/menu');
 		} else {
