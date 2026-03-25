1cf7d444f03f9a0ad0cefafe859e47fc04d45b08
Konstantin <site@webdes.by>
2026-03-25 14:41:51 +0300

    Исправление ошибки при копировании товаров

--- public/admin/model/catalog/product.php
+++ public/admin/model/catalog/product.php
@@ -260,7 +260,7 @@ class ModelCatalogProduct extends Model {
 			$data['product_layout'] = $this->getProductLayouts($product_id);
 			$data['product_store'] = $this->getProductStores($product_id);
 			$data['product_recurrings'] = $this->getRecurrings($product_id);
-
+			unset($data['product_id']);
 			$this->addProduct($data);
 		}
 	}
