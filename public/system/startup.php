f4a83048f725810e9c9aa25e83922b9950cfeef0
Konstantin <site@webdes.by>
2026-05-11 11:04:54 +0300

    🐛 fix(startup): обновить минимальную версию PHP до 8.5.0

--- public/system/startup.php
+++ public/system/startup.php
@@ -3,8 +3,8 @@
 error_reporting(E_ALL);
 
 // Check Version
-if (version_compare(phpversion(), '7.3.0', '<') == true) {
-	exit('PH7.3+ Required');
+if (version_compare(phpversion(), '8.5.0', '<') == true) {
+	exit('PHP8.5+ Required');
 }
 
 if (!ini_get('date.timezone')) {
