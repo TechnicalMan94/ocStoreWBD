34219cddc12106de33ef1179f31037ebc7134f73
Konstantin <site@webdes.by>
2026-05-12 00:49:26 +0300

    Рефакторинг генерации карты сайта: перемещение блока с продуктами для улучшения структуры и производительности.

--- public/catalog/controller/extension/feed/google_sitemap.php
+++ public/catalog/controller/extension/feed/google_sitemap.php
@@ -8,6 +8,36 @@ class ControllerExtensionFeedGoogleSitemap extends Controller {
 			$this->load->model('catalog/product');
 			$this->load->model('tool/image');
 
+			$this->load->model('catalog/category');
+
+			$output .= $this->getCategories(0);
+
+			$this->load->model('catalog/manufacturer');
+
+			$manufacturers = $this->model_catalog_manufacturer->getManufacturers();
+
+			foreach ($manufacturers as $manufacturer) {
+				$output .= '<url>';
+				$output .= '  <loc>' . $this->url->link('product/manufacturer/info', 'manufacturer_id=' . $manufacturer['manufacturer_id']) . '</loc>';
+				$output .= '  <changefreq>weekly</changefreq>';
+				$output .= '  <priority>0.7</priority>';
+				$output .= '</url>';
+
+				
+			}
+
+			$this->load->model('catalog/information');
+
+			$informations = $this->model_catalog_information->getInformations();
+
+			foreach ($informations as $information) {
+				$output .= '<url>';
+				$output .= '  <loc>' . $this->url->link('information/information', 'information_id=' . $information['information_id']) . '</loc>';
+				$output .= '  <changefreq>weekly</changefreq>';
+				$output .= '  <priority>0.5</priority>';
+				$output .= '</url>';
+			}
+
 			$products = $this->model_catalog_product->getProducts();
 
 			foreach ($products as $product) {
@@ -26,43 +56,7 @@ class ControllerExtensionFeedGoogleSitemap extends Controller {
 				}
 			}
 
-			$this->load->model('catalog/category');
-
-			$output .= $this->getCategories(0);
-
-			$this->load->model('catalog/manufacturer');
-
-			$manufacturers = $this->model_catalog_manufacturer->getManufacturers();
-
-			foreach ($manufacturers as $manufacturer) {
-				$output .= '<url>';
-				$output .= '  <loc>' . $this->url->link('product/manufacturer/info', 'manufacturer_id=' . $manufacturer['manufacturer_id']) . '</loc>';
-				$output .= '  <changefreq>weekly</changefreq>';
-				$output .= '  <priority>0.7</priority>';
-				$output .= '</url>';
-
-				$products = $this->model_catalog_product->getProducts(array('filter_manufacturer_id' => $manufacturer['manufacturer_id']));
-
-				foreach ($products as $product) {
-					$output .= '<url>';
-					$output .= '  <loc>' . $this->url->link('product/product', 'manufacturer_id=' . $manufacturer['manufacturer_id'] . '&product_id=' . $product['product_id'] . (!empty($product['variant_key']) ? '&variant_key=' . $product['variant_key'] : '')) . '</loc>';
-					$output .= '  <changefreq>weekly</changefreq>';
-					$output .= '  <priority>1.0</priority>';
-					$output .= '</url>';
-				}
-			}
-
-			$this->load->model('catalog/information');
-
-			$informations = $this->model_catalog_information->getInformations();
-
-			foreach ($informations as $information) {
-				$output .= '<url>';
-				$output .= '  <loc>' . $this->url->link('information/information', 'information_id=' . $information['information_id']) . '</loc>';
-				$output .= '  <changefreq>weekly</changefreq>';
-				$output .= '  <priority>0.5</priority>';
-				$output .= '</url>';
-			}
+			
 
 			$output .= '</urlset>';
 
@@ -88,16 +82,6 @@ class ControllerExtensionFeedGoogleSitemap extends Controller {
 			$output .= '  <changefreq>weekly</changefreq>';
 			$output .= '  <priority>0.7</priority>';
 			$output .= '</url>';
-
-			$products = $this->model_catalog_product->getProducts(array('filter_category_id' => $result['category_id']));
-
-			foreach ($products as $product) {
-				$output .= '<url>';
-				$output .= '  <loc>' . $this->url->link('product/product', 'path=' . $new_path . '&product_id=' . $product['product_id'] . (!empty($product['variant_key']) ? '&variant_key=' . $product['variant_key'] : '')) . '</loc>';
-				$output .= '  <changefreq>weekly</changefreq>';
-				$output .= '  <priority>1.0</priority>';
-				$output .= '</url>';
-			}
 
 			$output .= $this->getCategories($result['category_id'], $new_path);
 		}
