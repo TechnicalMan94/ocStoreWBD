47d8df456d196c070d55f4c25c46948cc8f5cb91
Konstantin <site@webdes.by>
2026-05-12 01:20:57 +0300

    Обновить генерацию карты сайта: добавить поддержку изображений, частоты изменений и даты последнего изменения для продуктов.

--- public/catalog/controller/extension/feed/google_sitemap.php
+++ public/catalog/controller/extension/feed/google_sitemap.php
@@ -1,12 +1,32 @@
 <?php
-class ControllerExtensionFeedGoogleSitemap extends Controller {
-	public function index() {
+class ControllerExtensionFeedGoogleSitemap extends Controller
+{
+	public function index()
+	{
 		if ($this->config->get('feed_google_sitemap_status')) {
 			$output  = '<?xml version="1.0" encoding="UTF-8"?>';
 			$output .= '<urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9" xmlns:image="http://www.google.com/schemas/sitemap-image/1.1">';
 
 			$this->load->model('catalog/product');
 			$this->load->model('tool/image');
+
+			$products = $this->model_catalog_product->getProducts();
+
+			foreach ($products as $product) {
+				$output .= '<url>';
+				$output .= '  <loc>' . $this->url->link('product/product', 'product_id=' . $product['product_id'] . (!empty($product['variant_key']) ? '&variant_key=' . $product['variant_key'] : '')) . '</loc>';
+				$output .= '  <changefreq>weekly</changefreq>';
+				$output .= '  <lastmod>' . date('Y-m-d\TH:i:sP', strtotime($product['date_modified'])) . '</lastmod>';
+				$output .= '  <priority>1.0</priority>';
+				if ($product['image']) {
+					$output .= '  <image:image>';
+					$output .= '  <image:loc>' . $this->model_tool_image->resize($product['image'], $this->config->get('theme_default_image_popup_width'), $this->config->get('theme_default_image_popup_height')) . '</image:loc>';
+					$output .= '  <image:caption>' . $product['name'] . '</image:caption>';
+					$output .= '  <image:title>' . $product['name'] . '</image:title>';
+					$output .= '  </image:image>';
+				}
+				$output .= '</url>';
+			}
 
 			$this->load->model('catalog/category');
 
@@ -22,8 +42,6 @@ class ControllerExtensionFeedGoogleSitemap extends Controller {
 				$output .= '  <changefreq>weekly</changefreq>';
 				$output .= '  <priority>0.7</priority>';
 				$output .= '</url>';
-
-				
 			}
 
 			$this->load->model('catalog/information');
@@ -38,26 +56,6 @@ class ControllerExtensionFeedGoogleSitemap extends Controller {
 				$output .= '</url>';
 			}
 
-			$products = $this->model_catalog_product->getProducts();
-
-			foreach ($products as $product) {
-				if ($product['image']) {
-					$output .= '<url>';
-					$output .= '  <loc>' . $this->url->link('product/product', 'product_id=' . $product['product_id'] . (!empty($product['variant_key']) ? '&variant_key=' . $product['variant_key'] : '')) . '</loc>';
-					$output .= '  <changefreq>weekly</changefreq>';
-					$output .= '  <lastmod>' . date('Y-m-d\TH:i:sP', strtotime($product['date_modified'])) . '</lastmod>';
-					$output .= '  <priority>1.0</priority>';
-					$output .= '  <image:image>';
-					$output .= '  <image:loc>' . $this->model_tool_image->resize($product['image'], $this->config->get('theme_default_image_popup_width'), $this->config->get('theme_default_image_popup_height')) . '</image:loc>';
-					$output .= '  <image:caption>' . $product['name'] . '</image:caption>';
-					$output .= '  <image:title>' . $product['name'] . '</image:title>';
-					$output .= '  </image:image>';
-					$output .= '</url>';
-				}
-			}
-
-			
-
 			$output .= '</urlset>';
 
 			$this->response->addHeader('Content-Type: application/xml');
@@ -65,7 +63,8 @@ class ControllerExtensionFeedGoogleSitemap extends Controller {
 		}
 	}
 
-	protected function getCategories($parent_id, $current_path = '') {
+	protected function getCategories($parent_id, $current_path = '')
+	{
 		$output = '';
 
 		$results = $this->model_catalog_category->getCategories($parent_id);
